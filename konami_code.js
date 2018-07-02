c751d86e89035f07648fc353fb02f48924fa2cf5
Felix Chan <felixchan29@gmail.com>
2018-07-02 15:16:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,7 +14,7 @@ const codes = [
 function init() {
   // your code here
   let index = 0;
-  document.body.addEventListener("keydown", )
+  document.body.addEventListener("keydown", function)
     const key = e.key;
     console.log(key);
     if(key === codes[index]) {
