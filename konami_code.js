90a4394a93d9fccd94c2bd1ad03e197d8c5be6c5
Felix Chan <felixchan29@gmail.com>
2018-07-02 14:58:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,18 +14,19 @@ const codes = [
 function init() {
   // your code here
   let index = 0;
-  
-  const key = keyCode;
-  console.log(key);
-  if(key === codes[index]) {
-    index++;
-    console.log(index);
-    if(index === codes.length){
-      alert("Congrat!!")
-    
+  function onKeyDownHandler(e){
+    const key = keyCode;
+    console.log(key);
+    if(key === codes[index]) {
+      index++;
+      console.log(index);
+      if(index === codes.length){
+        alert("Congrat!!")
+      
+        index = 0;
+      }
+    } else {
       index = 0;
     }
-  } else {
-    index = 0;
   }
 }
